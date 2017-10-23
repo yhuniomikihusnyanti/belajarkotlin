46589a7fe8f30dc3fb51582618ef1c7ce3e62388
yhuniomikihusnyanti <yhuniohusna@gmail.com>
2017-10-23 11:25:32 +0700

    commit ke sepuluh

---
+++ src/global_and_local_variabel.kt
@@ -0,0 +1,10 @@
+var fullname="yhunio miki husnayanti" // global var
+fun display():Unit{
+    println(fullname)
+}
+
+fun main(args: Array<String>) {
+    var name="yhunio" //local var
+    println(fullname)
+    print("name:"+name)
+}
